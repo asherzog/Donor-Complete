53fb155dc9ff48c534a7a1d0d46a00c01c077fe4
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-14 14:28:16 -0700

    Home-page API calls and formated info

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  $.get('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?state=CO&costToCompleteRange=0+TO+200&sortBy=2&APIKey=DONORSCHOOSE', function(data) {
-    console.log(data);
+  $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?costToCompleteRange=0+TO+100&sortBy=2&description=true&concise=true&max=21&APIKey=DONORSCHOOSE', function(data) {
+    var $proposals = data["proposals"];
+    $('section').append('<div id="main" class="row grey lighten-1"></div>');
+    $proposals.forEach(function(item) {
+      $('#main').append('<div class="card-panel hoverable center-align" id=' + item['id'] + '></div>');
+      $('#' + item['id']).append('<figure></figure>');
+      $('#' + item['id'] +' figure').append('<img class="responsive-img z-depth-3" src='+ item['imageURL'] + '></img>');
+      $('#' + item['id']).append('<h5 class="grey-text text-darken-3">' + item['title'] + '</h5>');
+      $('#' + item['id']).append('<h6>' + item['city'] + ', ' + item['state'] + '</h6>');
+      $('#' + item['id']).append('<h4 class="green-text text-darken-2 center-align"> $' + item['costToComplete'] + '</h4>');
+      $('#' + item['id']).append('<p>' + item['shortdescription'] + '</p>');
+      $('#' + item['id']).append('<button type="button" class="waves-effect waves-light btn grey darken-2 hoverable">More Info</button>');
+    });
   });
 });
