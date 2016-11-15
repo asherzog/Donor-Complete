11fbb13d1803044fbd56bbebabf5412abfcdd5a0
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-15 13:58:11 -0700

    Bootstrap is cool

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?costToCompleteRange=0+TO+100&sortBy=2&description=true&concise=true&max=21&APIKey=DONORSCHOOSE',
   appendPage);
-  $('.parallax').parallax();
+  // $('.parallax').parallax();
 });
 
 
@@ -10,7 +10,19 @@ function appendPage(data) {
   $('.main').append('<div id="main" class="row grey lighten-1"></div>');
   $proposals.forEach(addStuffToPage);
 
-  $('button').click(moreInfo);
+  $('button').click(function() {
+    $('.modal-content').html('');
+    var $id = $(event.target)[0].parentElement.id;
+    $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?keywords=' + $id + '&APIKey=DONORSCHOOSE', function(data) {
+      var info = data['proposals'][0];
+      $('.modal-content').append('<h5 class="new grey-text text-darken-3"">' + info['schoolName'] + '</h5>');
+      $('.modal-content').append('<h5 class="new grey-text text-darken-3"">Teacher: ' + info['teacherName'] + '</h5>');
+      $('.modal-content').append('<h6 class="new"">Grade Level: ' + info['gradeLevel']['name'] + '</h6>');
+      $('.modal-content').append('<h6 class="new"">Poverty: ' + info['povertyLevel'] + '</h6>');
+      $('.modal-content').append('<h6 class="new""> Subject: ' + info['subject']['name'] + '</h6>');
+      $('.modal-content').append('<h6 class="new"">Type: ' + info['resource']['name'] + '</h6>');
+    });
+  });
 };
 
 
@@ -23,7 +35,7 @@ function addStuffToPage(item) {
   $(itemId).append('<h6>' + item['city'] + ', ' + item['state'] + '</h6>');
   $(itemId).append('<h4 class="green-text text-darken-2 center-align"> $' + item['costToComplete'] + '</h4>');
   $(itemId).append('<p>' + item['shortdescription'] + '</p>');
-  $(itemId).append('<button id=btn' + itemId + ' type="button" class= "btn grey darken-3 hoverable btn more'+ itemId + '">Explore</button>');
+  $(itemId).append('<button type="button" class="btn btn-primary" data-toggle="modal" data-target=.bs-example-modal-lg>Learn More</button>');
 
   $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?keywords=' + item['id'] + '&APIKey=DONORSCHOOSE', function(data) {
     var info = data['proposals'][0];
