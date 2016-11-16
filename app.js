227bd55f3330b5da346b0f09c6e429a1de8b225e
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-11-15 17:11:15 -0700

    End day2

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?costToCompleteRange=0+TO+100&sortBy=2&description=true&concise=true&max=21&APIKey=DONORSCHOOSE',
+  $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?costToCompleteRange=0+TO+100&sortBy=2&description=true&concise=true&max=12&APIKey=DONORSCHOOSE',
   appendPage);
-  // $('.parallax').parallax();
+
+  $('.parallax').parallax();
+
+
+
+
 });
 
 
@@ -10,45 +15,42 @@ function appendPage(data) {
   $('.main').append('<div id="main" class="row grey lighten-1"></div>');
   $proposals.forEach(addStuffToPage);
 
-  $('button').click(function() {
-    $('.modal-content').html('');
-    var $id = $(event.target)[0].parentElement.id;
-    $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?keywords=' + $id + '&APIKey=DONORSCHOOSE', function(data) {
-      var info = data['proposals'][0];
-      $('.modal-content').append('<h5 class="new grey-text text-darken-3"">' + info['schoolName'] + '</h5>');
-      $('.modal-content').append('<h5 class="new grey-text text-darken-3"">Teacher: ' + info['teacherName'] + '</h5>');
-      $('.modal-content').append('<h6 class="new"">Grade Level: ' + info['gradeLevel']['name'] + '</h6>');
-      $('.modal-content').append('<h6 class="new"">Poverty: ' + info['povertyLevel'] + '</h6>');
-      $('.modal-content').append('<h6 class="new""> Subject: ' + info['subject']['name'] + '</h6>');
-      $('.modal-content').append('<h6 class="new"">Type: ' + info['resource']['name'] + '</h6>');
-    });
-  });
+  $('.more').click(moreInfo);
 };
 
 
 function addStuffToPage(item) {
   var itemId = ('#' + item['id']);
   $('#main').append('<div class="card-panel hoverable center-align" id=' + item['id'] + '></div>');
-  $(itemId).append('<figure class="z-depth-4"></figure>');
-  $(itemId +' figure').append('<img class="responsive-img z-depth-3" src='+ item['imageURL'] + '></img>');
+  $(itemId).append('<img class="responsive-img z-depth-3" src='+ item['imageURL'] + ' style="max-width: 30vw;"></img>');
   $(itemId).append('<h5 class="grey-text text-darken-3">' + item['title'] + '</h5>');
   $(itemId).append('<h6>' + item['city'] + ', ' + item['state'] + '</h6>');
-  $(itemId).append('<h4 class="green-text text-darken-2 center-align"> $' + item['costToComplete'] + '</h4>');
+  $(itemId).append('<h4 class="green-text text-darken-2 center-align"> $' + item['costToComplete'] + '<br> Remaining</h4>');
   $(itemId).append('<p>' + item['shortdescription'] + '</p>');
-  $(itemId).append('<button type="button" class="btn btn-primary" data-toggle="modal" data-target=.bs-example-modal-lg>Learn More</button>');
+  $(itemId).append('<button id=btn' + itemId + ' type="button" class= "btn grey darken-3 hoverable btn more">Explore</button>');
 
   $.getJSON('https://galvanize-cors-proxy.herokuapp.com/https://api.donorschoose.org/common/json_feed.html?keywords=' + item['id'] + '&APIKey=DONORSCHOOSE', function(data) {
     var info = data['proposals'][0];
-    $(itemId).append('<h5 class="new grey-text text-darken-3" style="display: none">' + info['schoolName'] + '</h5>');
-    $(itemId).append('<h5 class="new grey-text text-darken-3" style="display: none">Teacher: ' + info['teacherName'] + '</h5>');
+    $(itemId).append('<h4 class="green-text text-darken-2 center-align" style="display: none"> $' + info['costToComplete'] + '<br> Remaining</h4>');
+    $(itemId).append('<h5 class="grey-text darken-3" style="display: none">' + info['percentFunded'] + '% funded</h5>');
+    $(itemId).append('<h5 class="new grey-text text-darken-3" style="display: none; text-decoration: underline">' + info['schoolName'] + '</h5>');
+    $(itemId).append('<h5 class="new grey-text text-darken-3" style="display: none">' + info['teacherName'] + '</h5>');
     $(itemId).append('<h6 class="new" style="display: none">Grade Level: ' + info['gradeLevel']['name'] + '</h6>');
     $(itemId).append('<h6 class="new" style="display: none">Poverty: ' + info['povertyLevel'] + '</h6>');
     $(itemId).append('<h6 class="new" style="display: none"> Subject: ' + info['subject']['name'] + '</h6>');
     $(itemId).append('<h6 class="new" style="display: none">Type: ' + info['resource']['name'] + '</h6>');
+    $(itemId).append('<a class="btn-large green darken-2 hoverable" href=' + info['fundURL'] + ' style="display: none; margin-bottom: 1em;">DONATE</a>');
+    $(itemId).append('<img src=' + info['retinaImageURL'] + ' style="display: none; max-width: 30vw;">');
   });
 };
 
 function moreInfo() {
   var $id = $(event.target)[0].id.replace(/\D/g,'');
   $('#' + $id + ' button').siblings().toggle('display');
+  if ($('#' + $id + ' button').html() == 'Explore') {
+    $('#' + $id + ' button').html('Back');
+  }
+  else {
+    $('#' + $id + ' button').html('Explore');
+  }
 };
